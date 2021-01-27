cb463520a90634aeb648ae008f8a84059b5c8144
aravindmungara <aravind.mungara@gmail.com>
2021-01-27 09:35:16 -0500

    adding missing files

---
+++ QPCore/Controllers/WebElementController.cs
@@ -0,0 +1,65 @@
+﻿using DataBaseModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QPCore.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QPCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebElementController : ControllerBase
+    {
+        private WebElementService _webElementService;
+
+        public WebElementController(WebElementService webElementService)
+        {
+            _webElementService = webElementService;
+        }
+        // GET: api/<WebModelController>
+        [HttpGet]
+       [Route("ElementTree")]
+        public IEnumerable<WebPageGroup> Get()
+        {
+            return _webElementService.GetWebElementTree();
+        }
+
+        //// GET api/<WebModelController>/5
+        /// <summary>
+        /// returns webElement
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public WebElement Get(int id)
+        {
+            return _webElementService.GetWebElement(id);
+        }
+
+        // POST api/<WebModelController>
+        [HttpPost]
+        public WebElement Post(WebElement value)
+        {
+            return _webElementService.AddWebElement(value);
+        }
+
+        //// PUT api/<WebModelController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, WebModel value)
+        //{
+        //}
+
+        // DELETE api/<WebModelController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _webElementService.deleteElement(id);
+        }
+    }
+}
---
+++ QPCore/Service/WebElementService.cs
@@ -0,0 +1,90 @@
+﻿using AutomationAssistant.Models.AppConfig;
+using DataBaseModel;
+using Newtonsoft.Json;
+using Npgsql;
+using NpgsqlTypes;
+using QPCore.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QPCore.Service
+{
+    public class WebElementService
+    {
+        PostgresDataBase _postgresDataBase;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="postgresDataBase"></param>
+        public WebElementService(PostgresDataBase postgresDataBase)
+        {
+            _postgresDataBase = postgresDataBase;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<WebPageGroup> GetWebElementTree()
+        {
+            var _data = _postgresDataBase.Procedure("getwebelementtree").ToList().FirstOrDefault();
+             List<WebPageGroup>  webModel = JsonConvert.DeserializeObject<List<WebPageGroup>>(_data.getwebelementtree);
+
+            return webModel;
+          }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public WebElement GetWebElement(int id)
+        {
+            var json = _postgresDataBase.Procedure("getElement", new { elementId = id }).ToList().FirstOrDefault();
+            WebElement webModel = JsonConvert.DeserializeObject<WebElement>(json.getmodeljson);
+
+            return webModel;
+        }
+
+        internal WebElement AddWebElement(WebElement value)
+        {
+            //addorUpdateWebElement
+
+            var json = _postgresDataBase.Procedure("addorUpdateWebElement", new {
+
+                elementid = value.elementid,
+                elementaliasname = value.elementaliasname,
+                elementtype = value.elementtype,
+                itype = value.itype,
+                ivalue = value.ivalue,
+                framenavigation = value.framenavigation,
+                command = value.command,
+                locationpath = value.locationpath,
+                screenshot = value.screenshot,
+                elementparentid = value.elementparentid,
+                applicationsection = value.applicationsection,
+                value = value.value,
+                pageid =  value.pageid
+
+            }).ToList().FirstOrDefault();
+
+            WebElement webModel = JsonConvert.DeserializeObject<WebElement>(json.addorupdatewebelement);
+
+            return webModel;
+
+
+
+            throw new NotImplementedException();
+        }
+        //deleteElement
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+
+        public void deleteElement(int id)
+        {
+            _postgresDataBase.Procedure("deleteElement", new { elementid = id }).ToList().FirstOrDefault();
+        }
+    }
+}
