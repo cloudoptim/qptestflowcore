1dfc6c3dc43f08abeb42259ce057886b4ec727b9
Chien Pham <chienpminh@gmail.com>
2021-05-03 22:44:24 +0700

    Add user information for checking locking testflow

--- QPCore/Model/Common/CheckLockingDTO.cs
+++ QPCore/Model/Common/CheckLockingDTO.cs
@@ -8,5 +8,8 @@ namespace QPCore.Model.Common
     public class CheckLockingDTO
     {
         public bool IsLocked { get; set; }
+        public string LockedByName { get; set; }
+        public int? LockedById { get; set; }
+        public string LockedByEmail { get; set; }
     }
 }
