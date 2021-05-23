f6cdeaf018b59cc8f9079ceb8f7dc6d7cfdc957e
Chien Pham <chienpminh@gmail.com>
2021-05-23 20:51:25 +0700

    add validation for create webpage

--- QPCore/Model/ViewModels/WebPageViewModel.cs
+++ QPCore/Model/ViewModels/WebPageViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using QPCore.Model.Common.Validations;
 
 namespace QPCore.Model.ViewModels
 {/// <summary>
@@ -13,6 +14,7 @@ namespace QPCore.Model.ViewModels
         public int pageId { get; set; }
         public int groupId { get; set; }
         [Required]
+        [IsNotNumber]
         public string pageName { get; set; }
         public string createdBy { get; set; }
         public Nullable<System.DateTime> createdDateTime { get; set; }
@@ -24,15 +26,15 @@ namespace QPCore.Model.ViewModels
     
     public class UpdatePageViewModel:CreatePageViewModel
     {
+        [Required]
         public int pageId { get; set; }
     }
     public class CreatePageViewModel
     {
         public int groupId { get; set; }
         [Required]
+        [IsNotNumber]
         public string pageName { get; set; }
         public string createdBy { get; set; }
-     
-
     }
 }
