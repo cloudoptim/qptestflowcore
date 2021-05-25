f995d8aef4c2f3c66dd1dbd1dd21710b9c50f9cf
Chien Pham <chienpminh@gmail.com>
2021-05-25 21:18:58 +0700

    fixed null value on parentfeatureid

--- QPCore/Model/DataBaseModel/AppFeature.cs
+++ QPCore/Model/DataBaseModel/AppFeature.cs
@@ -13,7 +13,7 @@ namespace QPCore.Model.DataBaseModel
         [Required]
         [IsNotNumber]
         public string FeatureName { get; set; }
-        public int ParentFeatureId { get; set; }
+        public int? ParentFeatureId { get; set; }
         public int ClientId { get; set; }
         public bool IsActive { get; set; }
         public List<Child> children { get; set; }
